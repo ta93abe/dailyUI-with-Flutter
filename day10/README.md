91ce2a91192793b46a5596d45d2022c9ab4d4e29
ta93abe <ta93psymagi@gmail.com>
2020-07-20 18:35:05 +0900

    day10 ver 1.0.0

--- day10/README.md
+++ day10/README.md
@@ -5,3 +5,4 @@ Social Share
 ## What I wanted to do
 
 - Share package (deep dive)
+- BottomModalSheet (I like this best of flutter widgets.)
--- day10/lib/main.dart
+++ day10/lib/main.dart
@@ -7,6 +7,8 @@ Design a social share button/icon and be mindful of the size, imagery, placement
  */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:share/share.dart';
 
 void main() => runApp(MyApp());
 
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Social Share',
+      debugShowCheckedModeBanner: false,
       home: SocialShare(),
     );
   }
@@ -25,10 +28,143 @@ class SocialShare extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Material App Bar'),
+        title: Text('Social Share'),
       ),
-      body: Center(
-        child: Text('Hello World'),
+      body: Column(
+        children: [
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FlatButton(
+                color: Colors.tealAccent,
+                child: Text("Share"),
+                onPressed: () {
+                  Share.share('check out my website https://ta93abe.com',
+                      subject: 'Look what I made!');
+                },
+              ),
+              FlatButton(
+                color: Colors.pinkAccent,
+                child: Text("Custom Share"),
+                onPressed: () {
+                  showModalBottomSheet(
+                    context: context,
+                    isScrollControlled: true,
+                    enableDrag: true,
+                    backgroundColor: Colors.white.withOpacity(0.8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    builder: (context) => Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        Row(
+                          children: [
+                            Spacer(),
+                            Column(
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 40),
+                                  child: FlatButton(
+                                    shape: CircleBorder(),
+                                    splashColor: Colors.transparent,
+                                    color: Colors.white,
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(),
+                                    child: Icon(Icons.close),
+                                  ),
+                                ),
+                                Spacer(),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            Spacer(),
+                            Container(
+                              width: 300,
+                              height: 300,
+                              decoration: BoxDecoration(
+                                color: Colors.pinkAccent,
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                            ),
+                            Spacer(),
+                            Text(
+                              "https://ta93abe.com",
+                            ),
+                            Spacer(),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                FlatButton(
+                                  onPressed: () {},
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(15),
+                                  color: Colors.lightBlueAccent,
+                                  child: Icon(
+                                    FontAwesome.twitter,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                FlatButton(
+                                  onPressed: () {},
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(15),
+                                  color: Colors.purple,
+                                  child: Icon(
+                                    FontAwesome.instagram,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                FlatButton(
+                                  onPressed: () {},
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(15),
+                                  color: Colors.blueAccent,
+                                  child: Icon(
+                                    FontAwesome.facebook,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                                FlatButton(
+                                  onPressed: () {},
+                                  shape: CircleBorder(),
+                                  padding: EdgeInsets.all(15),
+                                  color: Colors.red,
+                                  child: Icon(
+                                    FontAwesome.youtube_play,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Spacer(),
+                          ],
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("made with Flutter"),
+                FlutterLogo(),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
